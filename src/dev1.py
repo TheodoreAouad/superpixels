ce70cfedc20f87a6d8b1f89718e728a4eee779da
TheodoreAouad <theodoreaouad@hotmail.fr>
2020-11-23 20:45:36 +0100

    Fix hiersup. Add sortedlist handling (in comments)

--- src/dev1.py
+++ src/dev1.py
@@ -3,16 +3,16 @@ import matplotlib.pyplot as plt
 import src.plotter as p
 fig, ax = plt.subplots(1, 1, figsize=(15, 15))
 
-pathlib.Path(f'./results/hiersup/try_4/{len(self.superpixels)}/').mkdir(exist_ok=True, parents=True)
+pathlib.Path(f'./results/hiersup/try_6/{len(self.superpixels)}/').mkdir(exist_ok=True, parents=True)
 
-plt.imsave(f'./results/hiersup/try_4/{len(self.superpixels)}/sp.png', self.superpixels.array_means, cmap='gray')
-plt.imsave(f'./results/hiersup/try_4/{len(self.superpixels)}/img.png', self.superpixels.img_ini, cmap='gray')
+plt.imsave(f'./results/hiersup/try_6/{len(self.superpixels)}/sp.png', self.superpixels.array_means, cmap='gray')
+plt.imsave(f'./results/hiersup/try_6/{len(self.superpixels)}/img.png', self.superpixels.img_ini, cmap='gray')
 
 p.plot_img_mask_on_ax(ax, self.superpixels.array_means, self.superpixels.infer_superpixel_edges())
-fig.savefig(f'./results/hiersup/try_4/{len(self.superpixels)}/sp_frontier.png')
+fig.savefig(f'./results/hiersup/try_6/{len(self.superpixels)}/sp_frontier.png')
 
 p.plot_img_mask_on_ax(ax, self.superpixels.img_ini, self.superpixels.infer_superpixel_edges())
-fig.savefig(f'./results/hiersup/try_4/{len(self.superpixels)}/img_frontier.png')
+fig.savefig(f'./results/hiersup/try_6/{len(self.superpixels)}/img_frontier.png')
 
 
 import matplotlib.pyplot as plt
@@ -23,44 +23,30 @@ ax.set_yscale('log')
 ax.set_xscale('log')
 ax.set_xlabel('Nb neighbors')
 ax.set_ylabel('Time Update')
-fig.savefig('./results/hiersup/try_4/nei_time.png')
+fig.savefig('./results/hiersup/try_6/nei_time.png')
 
 
-fig, ax = plt.subplots(1, 1, figsize=(8, 8))
-ax.scatter(self.nb_weights_pop, self.time_weights)
-# ax.set_yscale('log')
-# ax.set_xscale('log')
-ax.set_xlabel('Nb weights pop')
-ax.set_ylabel('Weights finding time')
-fig.savefig('./results/hiersup/try_4/weights_pop_time.png')
 
 fig, ax = plt.subplots(1, 1, figsize=(8, 8))
 ax.plot(self.time_weights)
 ax.set_yscale('log')
-fig.savefig('./results/hiersup/try_4/time_weights.png')
+fig.savefig('./results/hiersup/try_6/time_weights.png')
 
 fig, ax = plt.subplots(1, 1, figsize=(8, 8))
 ax.plot(self.time_update)
 ax.set_yscale('log')
-fig.savefig('./results/hiersup/try_4/time_update.png')
+fig.savefig('./results/hiersup/try_6/time_update.png')
 
-
-fig, ax = plt.subplots(1, 1, figsize=(8, 8))
-ax.plot(self.nb_weights_pop)
-ax.set_yscale('log')
-ax.set_xscale('log')
-ax.grid(True)
-fig.savefig('./results/hiersup/try_4/nb_weights_pop.png')
 
 fig, ax = plt.subplots(1, 1, figsize=(8, 8))
 ax.plot(self.time_merge)
 ax.set_yscale('log')
-fig.savefig('./results/hiersup/try_4/time_merge.png')
+fig.savefig('./results/hiersup/try_6/time_merge.png')
 
 fig, ax = plt.subplots(1, 1, figsize=(8, 8))
 ax.plot(self.time_step)
 ax.set_yscale('log')
-fig.savefig('./results/hiersup/try_4/time_step.png')
+fig.savefig('./results/hiersup/try_6/time_step.png')
 
 
 fig, ax = plt.subplots(1, 1, figsize=(8, 8))
@@ -71,13 +57,30 @@ ax.plot(np.cumsum(self.time_merge), label='merge')
 ax.plot(np.cumsum(self.time_update) + np.cumsum(self.time_weights) + np.cumsum(self.time_merge), label='sum')
 ax.legend()
 ax.grid(True)
-fig.savefig('./results/hiersup/try_4/time_tot.png')
+fig.savefig('./results/hiersup/try_6/time_tot.png')
 
 
 fig, ax = plt.subplots(1, 1, figsize=(8, 8))
 ax.plot(self.nb_neighbors)
+fig.savefig('./results/hiersup/try_6/nb_neighbors.png')
+
+
+
+fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+ax.scatter(self.nb_weights_pop, self.time_weights)
+# ax.set_yscale('log')
+# ax.set_xscale('log')
+ax.set_xlabel('Nb weights pop')
+ax.set_ylabel('Weights finding time')
+fig.savefig('./results/hiersup/try_6/weights_pop_time.png')
+
+
+fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+ax.plot(self.nb_weights_pop)
 ax.set_yscale('log')
-fig.savefig('./results/hiersup/third_try/nb_neighbors.png')
+ax.set_xscale('log')
+ax.grid(True)
+fig.savefig('./results/hiersup/try_6/nb_weights_pop.png')
 
 
 from time import time
@@ -106,3 +109,10 @@ t_1 = time()
 for _ in range(10000000):
     daz = None
 du4 = time() - t_1
+
+
+for idx, (dist, key) in enumerate(self.weights):
+    if key == (256, 512):
+        print(idx)
+        break
+print('Done')
--- src/superpixel_models/hiersup.py
+++ src/superpixel_models/hiersup.py
@@ -4,18 +4,13 @@ from time import time
 import heapq
 
 import numpy as np
+from blist import sortedlist
 
 from src.superpixel_models.superpixel import SuperpixelImage
 
 
 @total_ordering
 class WeightItem(tuple):
-
-    def __eq__(self, other):
-        return self[0] == other[0]
-
-    def __ne__(self, other):
-        return not (self == other)
 
     def __lt__(self, other):
         return self[0] < other[0]
@@ -30,6 +25,7 @@ class Hiersup:
         self.W_ = None
         self.L_ = None
         self.weights = None
+        # self.weights_dict = None
         self.superpixels = None
         self.neighbors = None
         self.max_label = None
@@ -52,6 +48,8 @@ class Hiersup:
         labels = superpixels.array_label
         W, L = img.shape
         weights = []
+        # weights = sortedlist()
+        weights_dict = {}
         max_label = 0
 
         for idx1 in range(W - 1):
@@ -63,24 +61,60 @@ class Hiersup:
                 weights.append(WeightItem(
                     (dist1, (lb1, lb2))
                 ))
+                # wi = WeightItem(
+                #     (dist1, (lb1, lb2))
+                # )
+                # weights.add(wi)
+                # weights_dict[(lb1, lb2)] = wi
 
                 dist2 = distance(sp1, sp3)
                 weights.append(WeightItem(
                     (dist2, (lb1, lb3))
                 ))
+                # wi = WeightItem(
+                #     (dist2, (lb1, lb3))
+                # )
+                # weights.add(wi)
+                # weights_dict[(lb1, lb3)] = wi
 
                 max_label = max([max_label, lb1, lb2, lb3])
 
-        # Add last pixel that was not included above.
-        sp1, sp2 = img[-1, -2], img[-1, -1]
-        lb1, lb2 = labels[-1, -2], labels[-1, -1]
-        dist = distance(sp1, sp2)
-        weights.append(WeightItem((dist, (lb1, lb2))))
-        max_label = max([max_label, lb1, lb2])
+        # Add last pixels that were not included above.
+        for idx2 in range(L - 1):
+            sp1, sp2 = img[-1, idx2], img[-1, idx2 + 1]
+            lb1, lb2 = labels[-1, idx2], labels[-1, idx2 + 1]
+            dist = distance(sp1, sp2)
+            weights.append(WeightItem((dist, (lb1, lb2))))
+            
+            # wi = WeightItem((dist, (lb1, lb2)))
+            # weights.add(wi)
+            # weights_dict[(lb1, lb2)] = wi
+            
+            max_label = max([max_label, lb1, lb2])
+
+        for idx1 in range(W - 1):
+            sp1, sp2 = img[idx1, -1], img[idx1 + 1, -1]
+            lb1, lb2 = labels[idx1, -1], labels[idx1 + 1, -1]
+            dist = distance(sp1, sp2)
+            weights.append(WeightItem((dist, (lb1, lb2))))
+            
+            # wi = WeightItem((dist, (lb1, lb2)))
+            # weights.add(wi)
+            # weights_dict[(lb1, lb2)] = wi
+            
+            max_label = max([max_label, lb1, lb2])
+
+        # sp1, sp2 = img[-1, -2], img[-1, -1]
+        # lb1, lb2 = labels[-1, -2], labels[-1, -1]
+        # dist = distance(sp1, sp2)
+        # # weights.append(WeightItem((dist, (lb1, lb2))))
+        # wi = WeightItem((dist, (lb1, lb2)))
+        # weights.add(wi)
+        # weights_dict[(lb1, lb2)] = wi
 
         heapq.heapify(weights)
 
-        return weights, max_label
+        return weights, weights_dict, max_label
 
     @staticmethod
     def compute_neighbors(superpixels) -> Dict:
@@ -119,24 +153,35 @@ class Hiersup:
         return neighbors
 
     def update_weights(self, new_label: int, ):
-        labels = set(
-            [(new_label, lb2) for lb2 in self.neighbors[new_label]]
-        )
+        # labels = set(
+        #     [(new_label, lb2) for lb2 in self.neighbors[new_label]]
+        # )
 
-        for lb1, lb2 in labels:
+        for lb2 in self.neighbors[new_label]:
             t0 = time()
             value = self.distance(
-                self.superpixels[lb1].value,
+                self.superpixels[new_label].value,
                 self.superpixels[lb2].value
             )
 
             t1 = time()
-            heapq.heappush(self.weights, WeightItem((value, (lb1, lb2))))
+            heapq.heappush(self.weights, WeightItem((value, (new_label, lb2))))
+            # wi = WeightItem((value, (new_label, lb2)))
+            # self.weights.add(wi)
+            # self.weights_dict[(new_label, lb2)] = wi
             t3 = time()
 
             # print("UPDATE: Computing distance:", t1 - t0)
             # print("UPDATE: Getting index:", t2 - t1)
             # print("UPDATE: Inserting value:", t3 - t2)
+
+# 65279, 65535
+    def delete_weights(self, lb: int):
+        for lb2 in self.neighbors[lb]:
+            key = (lb, lb2) if (lb, lb2) in self.weights_dict.keys() else (lb2, lb)
+            self.weights.remove(self.weights_dict[key])
+            self.neighbors[lb2].remove(lb)
+            del(self.weights_dict[key])
 
 
     def merge_verteces(self):
@@ -147,7 +192,7 @@ class Hiersup:
         return (c1 - c2) ** 2
 
     def init_graph(self):
-        self.weights, self.max_label = self.compute_weights(self.superpixels, self.distance)
+        self.weights, self.weights_dict, self.max_label = self.compute_weights(self.superpixels, self.distance)
         self.neighbors = self.compute_neighbors(self.superpixels)
 
     def fit(
@@ -199,15 +244,19 @@ class Hiersup:
         self.merged_away.add(label1)
         self.merged_away.add(label2)
 
-        t1 = time()
+        # dist, (label1, label2) = self.weights[0]
+
         self.max_label += 1
+        t1 = time()
         self.superpixels.merge(label1, label2, label_out=self.max_label)
-        t2 = time()
         self.neighbors[self.max_label] = (
             self.neighbors[label1]
             .union(self.neighbors[label2])
             .difference([label1, label2])
         )
+        # self.delete_weights(label1)
+        # self.delete_weights(label2)
+        t2 = time()
         for lb in [label1, label2]:
             for nei in self.neighbors[lb].difference([label1, label2]):
                 self.neighbors[nei] = self.neighbors[nei].union([self.max_label]).difference([lb])
